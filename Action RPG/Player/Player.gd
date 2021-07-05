9c45645c69c370eddc427fa5ad9f6522217e2f2e
bruceherbert <bruce@ubergrafik.com>
2021-07-05 17:16:11 +1000

    done #10!
    part 10

--- Action RPG/Player/Player.gd
+++ Action RPG/Player/Player.gd
@@ -6,6 +6,12 @@ const MAX_SPEED = 80
 
 const FRICTION = 500
 
+# enum - sets a set of constants - and gives it a value - 0, 1, 2 etc.
+enum{
+	MOVE,
+	ROLL,
+	ATTACK
+}
 
 # 2-element structure that can be used to represent positions in 2D space or any other pair of numeric values.
 # Note: In a boolean context, a Vector2 will evaluate to false if it's equal to Vector2(0, 0). Otherwise, a Vector2 will always evaluate to true.
@@ -13,11 +19,28 @@ const FRICTION = 500
 # This is declared in the scope by adding it in here...
 var velocity = Vector2.ZERO
 
+var state = MOVE
+
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
-func _physics_process(delta):
+func _ready():
+	animationTree.active = true
+
+func _process(delta):
+	match state:
+		MOVE:
+			move_state(delta)
+		
+		ROLL:
+			pass
+		
+		ATTACK:
+			attack_state(delta)
+
+
+func move_state(delta):
 	# Create a variable to store the input vectors in - use the 'Vector2' as a 'template' to do this
 	var input_vector = Vector2.ZERO
 	# Add values into the variable
@@ -33,6 +56,7 @@ func _physics_process(delta):
 	if input_vector != Vector2.ZERO:
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
+		animationTree.set("parameters/Attack/blend_position", input_vector)
 		animationState.travel("Run")
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
@@ -48,3 +72,13 @@ func _physics_process(delta):
 	# BUT, it makes it REALLY slow on an ok machine, so multiply it by MAX_SPEED
 	# Want to aim for pixels per second, rather than frame rate.
 	velocity = move_and_slide(velocity)
+	
+	if Input.is_action_just_pressed("attack"):
+		state = ATTACK
+
+func attack_state(delta):
+	velocity = Vector2.ZERO
+	animationState.travel("Attack")
+
+func attack_animation_finished():
+	state = MOVE
