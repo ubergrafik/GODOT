6ed22912a5d046755988d07e6df6bb9619df5f46
bruceherbert <bruce@ubergrafik.com>
2021-07-14 16:32:09 +1000

    part 18

---
+++ godot_scorm/btn_getpassfail.gd
@@ -0,0 +1,24 @@
+extends Button
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+signal return_successStatus(status)
+
+func _on_btn_getpassfail_pressed():
+	var valueReturn = JavaScript.eval("""
+		ScormProcessGetValue('cmi.success_status');
+	""")
+	emit_signal("return_successStatus", valueReturn)
---
+++ godot_scorm/btn_getstatus.gd
@@ -0,0 +1,24 @@
+extends Button
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+signal return_completionStatus(status)
+
+func _on_btn_getstatus_pressed():
+	var valueReturn = JavaScript.eval("""
+		ScormProcessGetValue('cmi.completion_status');
+	""")
+	emit_signal("return_completionStatus", valueReturn)
