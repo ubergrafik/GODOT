7a05f396a46b788580c44026ff2d3aa4beef6a6f
bruceherbert <bruce@ubergrafik.com>
2021-08-16 14:43:06 +1000

    ARPG v3 Part 1
    Part 1 complete

--- ARPG v 3/Player.gd
+++ ARPG v 3/Player.gd
@@ -2,31 +2,29 @@ extends KinematicBody2D
 
 var velocity = Vector2.ZERO
 
-const ACCELERATION = 500
-const MAX_SPEED = 80
-const FRICTION = 500
+export var ACCELERATION = 500
+export var MAX_SPEED = 80
+export var ROLL_SPEED = 120
+export var FRICTION = 500
 
 onready var animationPlayer = $AnimationPlayer
 onready var animationTree = $AnimationTree
 onready var animationState = animationTree.get("parameters/playback")
 
-func _ready():
-	animationTree.active = true
-
-func _physics_process(delta):
+func _process(delta):
 	var input_vector = Vector2.ZERO
 	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
 	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
 	input_vector = input_vector.normalized()
-	
+		
 	if input_vector != Vector2.ZERO:
 		animationTree.set("parameters/Idle/blend_position", input_vector)
 		animationTree.set("parameters/Run/blend_position", input_vector)
 		animationState.travel("Run")
+
 		velocity = velocity.move_toward(input_vector * MAX_SPEED, ACCELERATION * delta)
 	else:
 		animationState.travel("Idle")
 		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
 		
-# warning-ignore:return_value_discarded
-	move_and_slide(velocity)	
+	velocity = move_and_slide(velocity)
