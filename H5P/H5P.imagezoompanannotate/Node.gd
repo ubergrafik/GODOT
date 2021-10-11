be5296d7b99a52553b5205f3c7945f0fd6daa31b
bruceherbert <bruce@ubergrafik.com>
2021-10-11 12:29:54 +1100

    new stuff

--- H5P/H5P.imagezoompanannotate/Node.gd
+++ H5P/H5P.imagezoompanannotate/Node.gd
@@ -1,63 +1,33 @@
-extends Node
+extends Node2D
 
 # Version 1 - image zoom and pan 
-# Allows zoom in (up to 4 times) 
-# Allows zoom out (back to starting magnification) 
-# Allows dragging/panning of image 
+# Allows zoom in (up
 
-
-# Define max zoom in/out
-# Define extents of panning
-# Active panel
-# Define key bindings
-onready var paneA = $Pane_A_image
-onready var paneAzoomfactor = $Pane_A_zoom_factor
-onready var zoom_factor = 0.1
-const MAX_ZOOM = 0
-const MIN_ZOOM = 0.4
-const PAN_FACTOR = 10
-onready var pane_a_current_zoom = zoom_factor
-onready var pane_b_current_zoom = zoom_factor
+onready var paneA = $paneA
+onready var paneB = $paneB
 
 func _ready():
-	updateZoomFactorText()
+	panel_active(true, false)
 
-func _on_Button_zoom_in_pressed():
-	print("zoom IN pressed")
-	zoom("in")
+func _on_Button_Pane_A_active_pressed():
+	panel_active(true, false)
 
-func _on_Button_zoom_out_pressed():
-	print("zoom OUT pressed")
-	zoom("out")
-
+func _on_Button_Pane_B_active_pressed():
+	panel_active(false, true)
+	
 func _input(event):
-	if event.is_action_pressed("zoom_in"):
-		zoom("in")
-	if event.is_action_pressed("zoom_out"):
-		zoom("out")
-	
+	if event.is_action_pressed("paneA_active"):
+		panel_active(true, false)
+	if event.is_action_pressed("paneB_active"):
+		panel_active(false, true)
 
-func _process(delta):
-	var input_vector = Vector2.ZERO
-	input_vector.x = Input.get_action_strength("pan_right") - Input.get_action_strength("pan_left")
-	input_vector.y = Input.get_action_strength("pan_down") - Input.get_action_strength("pan_up")
-	input_vector = input_vector.normalized()
-	
-	var moveH = input_vector.x * PAN_FACTOR
-	var moveV = input_vector.y * PAN_FACTOR	
-	paneA.position.x += moveH
-	paneA.position.y += moveV
-
-func zoom(direction):
-	if direction == "in":
-		pane_a_current_zoom += zoom_factor
-		paneA.scale.x = pane_a_current_zoom
-		paneA.scale.y = pane_a_current_zoom
-	if direction == "out":
-		pane_a_current_zoom -= zoom_factor
-		paneA.scale.x = pane_a_current_zoom
-		paneA.scale.y = pane_a_current_zoom
-	updateZoomFactorText()
-	
-func updateZoomFactorText():
-	paneAzoomfactor.text = str("Zoom: ", pane_a_current_zoom)
+func panel_active(paneA_active, paneB_active):
+	if paneA_active && !paneB_active:
+		paneA.isActive = true
+		paneB.isActive = false
+	if !paneA_active && paneB_active:
+		paneA.isActive = false
+		paneB.isActive = true
+	if !paneA_active && !paneB_active:
+		paneA.isActive = false
+		paneB.isActive = false
---
+++ H5P/H5P.imagezoompanannotate/panel.gd
@@ -0,0 +1,90 @@
+extends Node2D
+
+# Version 1 - image zoom and pan 
+# Allows zoom in (up to 4 times) 
+# Allows zoom out (back to starting magnification) 
+# Allows dragging/panning of image 
+
+# Define max zoom in/out
+# Define extents of panning
+# Active panel
+# Define key bindings
+onready var isActive = false
+onready var image = $Image
+onready var zoomFactorText = $Zoom_factor_text
+onready var zoom_factor = 0.1
+const ZOOM_STEPS = 5
+const MAX_ZOOM = 0.3
+const MIN_ZOOM = 0.1
+const PAN_FACTOR = 10
+const VIEWPORT_WIDTH = 512
+const VIEWPORT_HEIGHT = 600
+onready var current_zoom = zoom_factor
+var image_draggable = false
+
+func _ready():
+	updateZoomFactorText()
+
+func _on_Button_zoom_in_pressed():
+	if isActive:
+		zoom("in")
+
+func _on_Button_zoom_out_pressed():
+	if isActive:
+			zoom("out")
+
+func _on_Area2D_input_event(viewport, event, shape_idx):
+	if isActive:
+		if Input.is_action_just_pressed("click"):
+			image_draggable = true
+
+func _input(event):
+	if isActive:
+		if event.is_action_pressed("zoom_in"):
+			zoom("in")
+		if event.is_action_pressed("zoom_out"):
+			zoom("out")
+		if event is InputEventMouseButton:
+			if event.button_index == BUTTON_LEFT and not event.pressed:
+				image_draggable = false
+
+func _process(delta):
+	if isActive:
+		var input_vector = Vector2.ZERO
+		input_vector.x = Input.get_action_strength("pan_right") - Input.get_action_strength("pan_left")
+		input_vector.y = Input.get_action_strength("pan_down") - Input.get_action_strength("pan_up")
+		input_vector = input_vector.normalized()
+
+		var moveH = input_vector.x * PAN_FACTOR
+		var moveV = input_vector.y * PAN_FACTOR	
+	
+		if image_draggable:
+			image.global_position = get_global_mouse_position()
+		
+		if image_draggable == false:
+			image.position.x += moveH
+			image.position.y += moveV
+
+func zoom(direction):
+	if direction == "in":
+		if current_zoom <= MAX_ZOOM:
+			current_zoom += zoom_factor
+			image.scale.x = current_zoom
+			image.scale.y = current_zoom
+	if direction == "out":
+		if current_zoom > MIN_ZOOM:
+			current_zoom -= zoom_factor
+			image.scale.x = current_zoom
+			image.scale.y = current_zoom
+	updateZoomFactorText()
+	
+func updateZoomFactorText():
+	zoomFactorText.text = str("Zoom: ", current_zoom)
+
+
+func _on_Button_draw_rect_pressed():
+	pass # Replace with function body.
+
+
+func _on_Button_draw_text_pressed():
+	pass # Replace with function body.
